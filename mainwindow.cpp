2425e8c9b000c13a8931a0fe65807577cca94e18
Max Portocarrero <mxportocarrero@gmail.com>
2016-09-27 07:47:12 -0500

    Primer Commit - Interfaz Terminada

---
+++ mainwindow.cpp
@@ -0,0 +1,21 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_FileBrowserButton_clicked()
+{
+    QString filename = QFileDialog::getOpenFileName(this, tr("Abrir archivo"), "/home", tr("TXT Files (*.txt)"));
+    ui->FilePathEdit->setText(filename);
+}
